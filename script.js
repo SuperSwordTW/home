51f10dfbb72daeb03315ff3c2ade646b9c9d26c2
SuperSwordTW <linjustin0209@gmail.com>
2022-06-02 17:03:11 +0800

    This is a webpage for Minecraft map

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("works");
+
+const checkbox = document.querySelector('button.checkbox');
+
+checkbox.addEventListener('click', (event) => {
+    if (checkbox.classList.contains('on')) {
+        checkbox.setAttribute('aria-checked', 'false');
+    }
+    else {
+        checkbox.setAttribute('aria-checked', 'true');
+    }
+    checkbox.classList.toggle('on');
+});
